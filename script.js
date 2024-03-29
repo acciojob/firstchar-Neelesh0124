510be07d936833f84479d5f9843fb9f177f0295c
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-03-29 10:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
   // your code here
+	for (let index = 0; index < bound; index++) {
+		if (text[i]!=' ') {
+			console.log(text[i]);
+		}
+	}
+	
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+//const text = prompt("Enter text:");
+//alert(firstChar(text));
